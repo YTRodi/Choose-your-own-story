f80a3bcd07f2c0524ad93d9cdb5dd3433e9feb07
YTRodi <yagorodi@gmail.com>
2022-08-21 23:11:41 -0300

    Initial Commit

---
+++ Own Story/Own_StoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Own_StoryApp.swift
+//  Own Story
+//
+//  Created by Yago Rodi on 21/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Own_StoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
